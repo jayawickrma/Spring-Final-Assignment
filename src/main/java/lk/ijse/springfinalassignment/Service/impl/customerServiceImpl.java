2fe72a462a8dbe4d173912c6a389bec3e92696b4
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-07 23:39:55 +0530

    customer service save mthod done

--- src/main/java/lk/ijse/springfinalassignment/Service/impl/customerServiceImpl.java
+++ src/main/java/lk/ijse/springfinalassignment/Service/impl/customerServiceImpl.java
@@ -2,17 +2,30 @@ package lk.ijse.springfinalassignment.Service.impl;
 
 import jakarta.persistence.Table;
 import jakarta.transaction.Transactional;
+import lk.ijse.springfinalassignment.DAO.CustomerDAO;
 import lk.ijse.springfinalassignment.DTO.impl.CustomerDTO;
+import lk.ijse.springfinalassignment.Entity.impl.CustomerEntity;
+import lk.ijse.springfinalassignment.Exeptions.DataPersistExeption;
 import lk.ijse.springfinalassignment.Service.CustomerService;
+import lk.ijse.springfinalassignment.Utill.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 @Transactional
 public class customerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerDAO customerDAO;
+    @Autowired
+    private Mapping mapping;
     @Override
     public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
-        return null;
+        CustomerEntity saveCustomer = customerDAO.save(mapping.toCustomerEntity(customerDTO));
+        if (saveCustomer==null){
+                throw new DataPersistExeption("something went wrong");
+        }
+        return mapping.toCustomerDto(saveCustomer);
     }
 
     @Override
