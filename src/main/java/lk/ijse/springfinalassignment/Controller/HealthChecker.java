4caaad923cda792bade3cbb9e7053a86e3b9068e
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-07 21:59:49 +0530

    health checker created

--- src/main/java/lk/ijse/springfinalassignment/Controller/HealthChecker.java
+++ src/main/java/lk/ijse/springfinalassignment/Controller/HealthChecker.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/a1/health")
+@RequestMapping("api/v3/health")
 public class HealthChecker {
     @GetMapping
     public String get(){
