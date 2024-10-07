4c198e002c712d33f32c258550589e7bd7d82f81
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-07 22:17:01 +0530

    app init class creatd and added their methods and multypartconfig  method

---
+++ src/main/java/lk/ijse/springfinalassignment/AppInit.java
@@ -0,0 +1,32 @@
+package lk.ijse.springfinalassignment;
+
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
+import lk.ijse.springfinalassignment.Config.WebAppConfig;
+import lk.ijse.springfinalassignment.Config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                WebAppRootConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                WebAppConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+//    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+//        String tempDir = System.getProperty("java.io.tmpdir");
+//        registration.setMultipartConfig(new MultipartConfigElement(tempDir));
+//    }
+}
