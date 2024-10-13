a270eca077a97c1dbc9da6cb6caae39d4f1b35b1
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-13 23:31:28 +0530

    itemService

--- src/main/java/lk/ijse/springfinalassignment/Service/impl/itemServiceImpl.java
+++ src/main/java/lk/ijse/springfinalassignment/Service/impl/itemServiceImpl.java
@@ -1,17 +1,30 @@
 package lk.ijse.springfinalassignment.Service.impl;
 
 import jakarta.transaction.Transactional;
+import lk.ijse.springfinalassignment.DAO.ItemDAO;
 import lk.ijse.springfinalassignment.DTO.impl.ItemDTO;
+import lk.ijse.springfinalassignment.Entity.impl.ItemEntity;
+import lk.ijse.springfinalassignment.Exeptions.DataPersistExeption;
 import lk.ijse.springfinalassignment.Service.ItemService;
+import lk.ijse.springfinalassignment.Utill.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 @Transactional
 public class itemServiceImpl implements ItemService {
+    @Autowired
+    private ItemDAO itemDAO;
+    @Autowired
+    private Mapping mapping;
     @Override
     public ItemDTO daveItem(ItemDTO itemDTO) {
-        return null;
+        ItemEntity saveItem =itemDAO.save(mapping.toItemEntity(itemDTO));
+            if (saveItem==null){
+                throw new DataPersistExeption("something went wrong");
+            }
+            return mapping.toItemDto(saveItem);
     }
 
     @Override
